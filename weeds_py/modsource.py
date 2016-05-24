1da0ec5d20e254efe1d7b0e5985d7ac532ab676b
Tom Rice <t.rice90@gmail.com>
2016-05-24 16:20:07 -0400

    comply with PEP8 (mostly automatic changes, and a function name change)

--- weeds_py/modsource.py
+++ weeds_py/modsource.py
@@ -16,8 +16,10 @@ from . import cdms
 from .db import blankPartfunc
 from .sicparse import OptionParser
 
+
 class component:
     pass
+
 
 def readmdl(filename):
     """
@@ -25,7 +27,7 @@ def readmdl(filename):
 
     Arguments
     filename -- name of the source model (".mdl")
-    
+
     """
 
     components = []
@@ -39,11 +41,11 @@ def readmdl(filename):
     # Set a parser for the optional fields
     parser = OptionParser()
     parser.add_option("-c", "--cache", action="store_false", dest="online",
-                      default=True) # unused
+                      default=True)  # unused
     parser.add_option("-a", "--absorption", action="store_true", dest="absorption",
                       default=False)
-    parser.add_option("-p", "--partfunc", dest = "partfunc", nargs = 1,
-                      type = "float", default = None)
+    parser.add_option("-p", "--partfunc", dest="partfunc", nargs=1,
+                      type="float", default=None)
 
     try:
         for line in f.readlines():
@@ -69,12 +71,12 @@ def readmdl(filename):
             c.theta = float(field[2])
             c.v_off = float(field[3])
             c.delta_v = float(field[4])
- 
+
             # Get optional fields
             if len(field) > 5:
                 (opts, args) = parser.parse_args(field[5:])
             else:
-                (opts, args) = parser.parse_args([]) # to set default values
+                (opts, args) = parser.parse_args([])  # to set default values
             if len(args) == 1:
                 c.origin = args[0]
             else:
@@ -82,17 +84,20 @@ def readmdl(filename):
             if len(args) > 1:
                 raise ValueError
             if not(opts.online):
-                pyclass.message(pyclass.seve.w, "MODSOURCE", "/CACHE option is obsolete (ignored).")
+                pyclass.message(
+                    pyclass.seve.w, "MODSOURCE", "/CACHE option is obsolete (ignored).")
             c.absorption = opts.absorption
-            c.partfunc = opts.partfunc # log10 of the partition function
+            c.partfunc = opts.partfunc  # log10 of the partition function
             c.keep_opacity = False
 
             components.append(c)
 
     except Exception:
-        raise Exception("Incorrect input on line %i of %s" % (linenumber, filename))
+        raise Exception("Incorrect input on line %i of %s" %
+                        (linenumber, filename))
 
     return components
+
 
 def J(T, freq):
     """
@@ -109,8 +114,8 @@ def J(T, freq):
     """
 
     J = planck_constant * freq * 1e6 / boltzmann_constant \
-            / (np.exp((planck_constant * freq * 1e6) \
-                               / (boltzmann_constant * T)) - 1)
+        / (np.exp((planck_constant * freq * 1e6)
+                  / (boltzmann_constant * T)) - 1)
 
     return J
 
@@ -125,54 +130,57 @@ def Planck_funct(T, freq):
 
     """
 
-    kk = 1.3807e-16 # Bolzmann's constant [erg/K]
-    hh = 6.6262e-27 # Planck's constant [erg.s]
-    cc  = 2.99792458e10  # Light speed [cm/s]
+    kk = 1.3807e-16  # Bolzmann's constant [erg/K]
+    hh = 6.6262e-27  # Planck's constant [erg.s]
+    cc = 2.99792458e10  # Light speed [cm/s]
 
-    
-    freq = freq*1e6 # MHz->Hz
+    freq = freq*1e6  # MHz->Hz
 
-    factor = 2.*hh/cc/cc*1e23 # Jy
+    factor = 2.*hh/cc/cc*1e23  # Jy
 
     bt = factor*freq**3/(np.exp(hh*freq/kk/T)-1.)
 
     return bt
 
 # TSR: I was told that K. Zhang wrote this function.
-def FindFreqStep(components,fmin):
+
+
+def find_freq_step(components, fmin):
     delta_v = []
     for c in components:
         delta_v.append(c.delta_v)
     min_delta_v = np.min(np.abs(np.array(delta_v)))
-    min_delta_f = min_delta_v * 1e3 / speed_of_light * fmin # MHz
+    min_delta_f = min_delta_v * 1e3 / speed_of_light * fmin  # MHz
     model_freq_step = min_delta_f / 10.
 
     return model_freq_step
 
 
-def getLines(cdmsobject,fmin,fmax,species,origin,energy = -1, einstein=-1):
-    
-    lines = cdmsobject.search(fmin,fmax,species=species,origin=origin)
+def getLines(cdmsobject, fmin, fmax, species, origin, energy=-1, einstein=-1):
+
+    lines = cdmsobject.search(fmin, fmax, species=species, origin=origin)
 
     return lines
 
-def getPartitionfuc(cdmsobject,species,Tex):
+
+def getPartitionfuc(cdmsobject, species, Tex):
     """get partition function, given species name and excitation temperature"""
-    t_dummy, part_dummy = cdmsobject.part_function(species,'cdms','cdms')
-    f = interp1d(np.log(t_dummy),np.log(part_dummy))
+    t_dummy, part_dummy = cdmsobject.part_function(species, 'cdms', 'cdms')
+    f = interp1d(np.log(t_dummy), np.log(part_dummy))
     return np.exp(f(np.log(Tex)))
 
-def modsource(components, fmin, fmax, freq_step = None, \
-              theta_tel = None, background = 2.7, \
-              verbose = False,extra_result = False):
+
+def modsource(components, fmin, fmax, freq_step=None,
+              theta_tel=None, background=2.7,
+              verbose=False, extra_result=False):
     """
     Model the emission of a given source at the ETL
 
     """
     if freq_step == None:
-        freq_step = FindFreqStep(components,fmin)
+        freq_step = find_freq_step(components, fmin)
 
-    freq = np.arange(fmin, fmax, freq_step)  
+    freq = np.arange(fmin, fmax, freq_step)
 
     # Compute the antenna temperature and opacity over the entire
     # frequency range.  See Maret et al. (2010, in prep.) for the
@@ -180,24 +188,22 @@ def modsource(components, fmin, fmax, freq_step = None, \
 
     tb_grand_tot = np.zeros(len(freq))  # All components, species and lines
     # TSR: I was told that K. Zhang added the `intensity_grand_tot` parameter.
-    intensity_grand_tot = np.zeros(len(freq)) 
-    
-    tb_species = np.zeros((len(freq),len(components)))
+    intensity_grand_tot = np.zeros(len(freq))
+
+    tb_species = np.zeros((len(freq), len(components)))
     keep_opacity_flag = False
     i = 0
 
-    ## a cdms object
+    # a cdms object
 
-    cdmsobject = cdms.Cdms(url = "http://www.astro.uni-koeln.de/cgi-bin/cdmssearch",
-          cache_file = "~/.gag/scratch/cdms.db", protocol = "cdms_post",
-          online = True, name = "cdms")
+    cdmsobject = cdms.Cdms(url="http://www.astro.uni-koeln.de/cgi-bin/cdmssearch",
+                           cache_file="~/.gag/scratch/cdms.db", protocol="cdms_post",
+                           online=True, name="cdms")
 
-    
     for c in components:
-        #print 'computing for species %s' %(c.species)
-        
-        lines = getLines(cdmsobject,fmin,fmax,c.species,c.origin,-1,-1)
-        
+        # print 'computing for species %s' %(c.species)
+
+        lines = getLines(cdmsobject, fmin, fmax, c.species, c.origin, -1, -1)
 
         if len(lines) == 0:
             print "No %s lines found in the frequency range" % (c.species)
@@ -206,8 +212,7 @@ def modsource(components, fmin, fmax, freq_step = None, \
         else:
             print " %i %s lines found in the frequency range" % (len(lines), c.species)
 
-        
-            partitionfunc= getPartitionfuc(cdmsobject,c.species,c.Tex)
+            partitionfunc = getPartitionfuc(cdmsobject, c.species, c.Tex)
 
             # Compute the total opacity for that species
 
@@ -215,55 +220,49 @@ def modsource(components, fmin, fmax, freq_step = None, \
             for l in lines:
 
                 # Line profile function
-                freq_off = -c.v_off * 1e3 / speed_of_light * l.frequency # MHz
+                freq_off = -c.v_off * 1e3 / speed_of_light * l.frequency  # MHz
                 sigma = l.frequency / (speed_of_light * np.sqrt(8 * np.log(2))) \
-                        * c.delta_v * 1e3 * 1e6 # Hz
-                phi = 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp (-((freq - l.frequency - freq_off) \
-                                                          * 1e6)**2 / (2 * sigma**2))
+                    * c.delta_v * 1e3 * 1e6  # Hz
+                phi = 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(-((freq - l.frequency - freq_off)
+                                                                  * 1e6)**2 / (2 * sigma**2))
 
                 # Line opacity
                 tau = speed_of_light**2 / (8 * np.pi * (freq * 1e6)**2) * l.einstein_coefficient \
-                      * c.Ntot * 1e4 * l.upper_level.statistical_weight \
-                      * np.exp(-l.upper_level.energy / c.Tex) \
-                      / partitionfunc * (np.exp(planck_constant * l.frequency * 1e6 \
-                                                                 / (c.Tex * boltzmann_constant))-1) * phi
+                    * c.Ntot * 1e4 * l.upper_level.statistical_weight \
+                    * np.exp(-l.upper_level.energy / c.Tex) \
+                    / partitionfunc * (np.exp(planck_constant * l.frequency * 1e6
+                                              / (c.Tex * boltzmann_constant))-1) * phi
 
                 # Opacity at line center
                 l.tau0 = max(tau)
-                
+
                 tau_tot = tau_tot + tau
-                
-                
+
                 if c.keep_opacity:
                     tau_kept = tau_tot
                     keep_opacity_flag = True
-        
-                    
 
-              
         # Compute the antenna temperature for that species
 
         Tbg = background
         eta_source = c.theta**2 / (theta_tel**2 + c.theta**2)
-        tb_tot = eta_source * (J(c.Tex, freq) - J(Tbg, freq)) * (1 - np.exp(-tau_tot))
-        
+        tb_tot = eta_source * \
+            (J(c.Tex, freq) - J(Tbg, freq)) * (1 - np.exp(-tau_tot))
+
         if not(c.absorption):
             tb_grand_tot = tb_grand_tot + tb_tot
             solid_angle = np.pi*c.theta**2/(206265.*206265.)
-            intensity_grand_tot = Planck_funct(c.Tex,freq)*(1.-np.exp(-tau_tot))*solid_angle+intensity_grand_tot
-            
+            intensity_grand_tot = Planck_funct(
+                c.Tex, freq)*(1.-np.exp(-tau_tot))*solid_angle+intensity_grand_tot
+
         else:
             tb_grand_tot = tb_grand_tot * np.exp(-tau_tot) + tb_tot
-            
 
-        tb_species[:,i] = tb_tot
-        i = i+1    
+        tb_species[:, i] = tb_tot
+        i = i+1
 
-        
-    # K. Zhang added this `extra_result` functionality.        
+    # K. Zhang added this `extra_result` functionality.
     if extra_result == True:
-        return freq, tb_grand_tot,tb_species,tau_tot,intensity_grand_tot
+        return freq, tb_grand_tot, tb_species, tau_tot, intensity_grand_tot
     else:
-        return freq, tb_grand_tot,tb_species
-
-
+        return freq, tb_grand_tot, tb_species
